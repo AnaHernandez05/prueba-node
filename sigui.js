d33cab3384b92e6c16c08e1e41f38da2b5d62d9f
AnaHernandez05 <hernandezgonzalezg7@gmail.com>
2021-05-25 14:41:49 -0500

    Agregando archivo nuevo

---
+++ sigui.js
@@ -0,0 +1,76 @@
+let autos = require('./autos');
+let persona = {
+        nombre: 'Juan',
+        capacidadDePagoEnCuotas: 30000,
+        capacidadDePagoTotal: 100000000
+};
+let concesionaria = {
+   autos: autos,
+   persona: persona,
+   buscarAuto: function(patenteB){
+    let autoB = autos.filter(function(autos){
+        auto = autos.patente;
+        return auto==patenteB;});
+        return autoB;
+},
+venderAuto: function (patenteA){
+    vender = this.buscarAuto(patenteA);
+    vender[0].vendido = true;
+    return vender;
+    },
+autosParaLaVenta: function(){
+    let vendidos = this.autos.filter(function(autos){
+    vendido = autos.vendido;
+    return vendido==false;});
+    return vendidos;
+  },
+  autosNuevos: function(){
+    let venta = this.autosParaLaVenta();
+    let nuevo = venta.filter(function(venta){
+        km = venta.km;
+        return km<=100;});
+        return nuevo;
+},   
+listaDeVentas: function (){
+    let ven = this.autos.filter(function(autos){
+        vendido = autos.vendido;
+        return vendido==true;});
+        let list = [];
+        for(let i=0; i<ven.length;i++){
+            if(ven[i].vendido == true){
+                list.push(ven[i].precio);
+            }
+        }
+        return list
+},
+    totalDeVentas:function(){
+        let total = this.listaDeVentas();
+        if(total.length >0){
+        var suma = total.reduce(function(acum, num){
+            return acum + num;
+           });       
+           return suma;
+    }else{
+        return 0;
+    }
+},
+   puedeComprar:function(auto,personas){
+       personas = this.persona;
+      if((personas.capacidadDePagoEnCuotas*auto.cuotas>auto.precio) && (personas.capacidadDePagoTotal>=auto.precio)){
+return true;
+      } else{
+         return false;
+      }
+   },
+   autosQuePuedeComprar: function(personas){
+      let autosDis = this.autosParaLaVenta();
+      return autosDis;
+   }
+};
+//console.log(concesionaria.venderAuto('JJK116'));
+//console.log(concesionaria.autosParaLaVenta());
+//console.log(concesionaria.autosNuevos());
+//console.log(concesionaria.listaDeVentas());
+//console.log(concesionaria.totalDeVentas());
+console.log(concesionaria.puedeComprar('Toyota','Juan'));
+//console.log(concesionaria.autosQuePuedeComprar());
